ff29766c78395f03a5ddf00d994d39532292edeb
H-H Moon <moonssang031@naver.com>
2021-05-21 05:19:41 +0900

    Initial commit

---
+++ app/src/main/java/gun0912/tedimagepicker/sample/MainActivity2.java
@@ -0,0 +1,13 @@
+package gun0912.tedimagepicker.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/gun0912/tedimagepicker/sample/viewActivity.java
@@ -0,0 +1,13 @@
+package gun0912.tedimagepicker.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
